92d9f77a8160836d2266dcfbb3aef6a7c046513f
Nicole Kay <nicolejkay@gmail.com>
2022-03-20 18:29:48 +0000

    changed zoom level

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidG90YWxseXdhbmRlcmxvc3QiLCJhIjoiY2wwejdpcHZkMG5lZzNqbzF0eGYwYzg2ZyJ9.e_DZl8WK_sJqxMy5hCJeqw';
 const map = L.map('map', {
     center: [51.511040, -0.103510], // Initial map position
-    zoom: 3,                        // Initial map zoom level
+    zoom: 5,                        // Initial map zoom level
     zoomControl: false,             // Disable zoom controls
     layers: [
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
