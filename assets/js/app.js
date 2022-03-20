031b4b02a3f9e80b46f79e457d2e6f783857d40b
James White <dev.jameswhite@gmail.com>
2022-03-20 19:47:21 +0000

    Fit map around pins bounding box

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,6 @@
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidG90YWxseXdhbmRlcmxvc3QiLCJhIjoiY2wwejdpcHZkMG5lZzNqbzF0eGYwYzg2ZyJ9.e_DZl8WK_sJqxMy5hCJeqw';
 const map = L.map('map', {
-    center: [51.511040, -0.103510], // Initial map position
-    zoom: 5,                        // Initial map zoom level
-    zoomControl: false,             // Disable zoom controls
+    zoomControl: false,
     layers: [
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -21,6 +19,9 @@ fetch('assets/data/journey.json').then(function (response) {
     }
 
     response.json().then(function (data) {
+        const locations = data.pins.map(p => p.location);
+        map.fitBounds(locations);
+
         for (const pin of data.pins) {
             L.marker(pin.location).addTo(map);
         }
