9c9c7d2b0efcb503ff78b4fc82de3fffe2930ef1
James White <dev.jameswhite@gmail.com>
2022-07-01 00:28:13 +0400

    Switch to endpoint that only returns published steps

--- data/fetch.py
+++ data/fetch.py
@@ -4,24 +4,6 @@ import json
 import requests
 
 
-def fetch(trip_id):
-    response = requests.get(f'https://api.polarsteps.com/trips/{trip_id}')
-    data = response.json()
-
-    return data
-
-def get_pins(trip_id):
-    data = fetch(trip_id)
-    steps = data['all_steps']
-
-    return [ get_pin(step) for step in steps]
-
-def get_pin(step):
-    return {
-        'name': step['location']['name'],
-        'location': [step['location']['lat'], step['location']['lon']]
-    }
-
 def update_data_file(trip_id, path):
     data = {}
     data['pins'] = get_pins(trip_id)
@@ -29,6 +11,24 @@ def update_data_file(trip_id, path):
     with open(path, 'wb') as file:
         contents = json.dumps(data, indent=4)
         file.write(contents.encode())
+
+def get_pins(trip_id):
+    data = fetch(trip_id)
+    steps = data['all_steps']
+
+    return [ get_pin(step) for step in steps]
+
+def fetch(trip_id):
+    response = requests.get(f'https://api.polarsteps.com/users/byusername/totallywanderlost')
+    trips = [trip for trip in response.json()['alltrips'] if trip['id'] == trip_id]
+
+    return trips[0]
+
+def get_pin(step):
+    return {
+        'name': step['location']['name'],
+        'location': [step['location']['lat'], step['location']['lon']]
+    }
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Fetch data from Polarsteps')
