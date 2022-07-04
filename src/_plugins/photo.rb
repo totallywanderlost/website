fe51d974ec58bfe451d057a5808df9d725e13b25
James White <dev.jameswhite@gmail.com>
2022-07-04 18:28:41 +1000

    Add photo_tag method

--- src/_plugins/photo.rb
+++ src/_plugins/photo.rb
@@ -9,6 +9,10 @@ module Jekyll
     def photo_url(path, config)
       "#{CDN}/#{path}?tr=w-#{config['width']},h-#{config['height']},fo-center"
     end
+
+    def photo_tag(path, config)
+      "<img loading=lazy src=\"#{photo_url(path, config)}\" width=#{config['width']} height=#{config['height']}>"
+    end
   end
 end
 
